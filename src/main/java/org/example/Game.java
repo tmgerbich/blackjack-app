8c4d5a6fa7580ff771babbbd1e699f254ed4556f
tmgerbich <therese.m.gerbich@gmail.com>
2024-08-01 15:18:32 -0400

    added betting

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -15,7 +15,6 @@ public class Game {
         this.wins = 0;
         this.losses = 0;
         this.draws = 0;
-        this.bet = 0;
         this.winnings = 0;
     }
 
@@ -24,6 +23,7 @@ public class Game {
         deck.shuffle();
         dealer.initialDeal(deck);
         player.initialDeal(deck);
+        player.setBet();
 
         // Handle Blackjack scenarios
         if (dealer.hasBlackjack() && player.hasBlackjack()) {
@@ -32,24 +32,28 @@ public class Game {
         } else if (dealer.hasBlackjack() && !player.hasBlackjack()) {
             System.out.println("You lose!");
             this.losses++;
+            this.winnings -= player.getBet();
         } else if (!dealer.hasBlackjack() && player.hasBlackjack()) {
             System.out.println("You win!");
             this.wins++;
+            this.winnings += player.getBet();
         } else {
             dealer.printHiddenHand();
             player.printHand();
-
+            player.doubleDown();
             player.makeDecision(deck);
             if (player.isBusted()) {
                 System.out.println(this.player.getName() + " busted!");
                 System.out.println("You lose!");
                 this.losses++;
+                this.winnings -= player.getBet();
             } else {
                 dealer.makeDecision(deck);
                 if (dealer.isBusted()) {
                     System.out.println(this.dealer.getName() + " busted!");
                     System.out.println("You win!");
                     this.wins++;
+                    this.winnings += player.getBet();
                 } else {
                     // Compare hands if neither busted
                     int playerValue = player.getHand().calculatedValue();
@@ -58,9 +62,11 @@ public class Game {
                     if (playerValue > dealerValue) {
                         System.out.println("You win!");
                         this.wins++;
+                        this.winnings += player.getBet();
                     } else if (playerValue < dealerValue) {
                         System.out.println("You lose!");
                         this.losses++;
+                        this.winnings -= player.getBet();
                     } else {
                         System.out.println("It's a tie!");
                         this.draws++;
@@ -73,5 +79,11 @@ public class Game {
 
     public void printScore() {
         System.out.println("At the end of this round you have " + this.wins + " Wins, " + this.losses + " Losses, and " + this.draws + " Draws");
+        if (this.winnings >= 0) {
+            System.out.println("Your winnings are $ " + this.winnings);
+        }
+        else {
+            System.out.println("You owe $ " + this.winnings);
+        }
     }
 }
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -2,20 +2,57 @@ package org.example;
 
 import java.util.Scanner;
 
-
-//implementation of blackjack player
+// Implementation of blackjack player
 public class Player extends Person {
+    Scanner scanner = new Scanner(System.in);
+    private int bet;
 
     public Player(String name) {
         super.setName(name);
+        this.bet = 0;
+    }
+
+    public int getBet() {
+        return this.bet;
+    }
+
+    public void setBet() {
+        while (true) {
+            System.out.println("What's your bet?");
+            int tryBet = scanner.nextInt();
+            if (tryBet % 5 == 0) {
+                this.bet = tryBet;
+                System.out.println("Your bet is: " + bet);
+                break;
+            } else {
+                System.out.println("Please bet in increments of 5");
+            }
+        }
+    }
+
+    public void doubleDown() {
+        while (true) {
+            System.out.println("Do you want to double down? y/n");
+            scanner.nextLine();
+            String response = scanner.nextLine();
+            if (response.equalsIgnoreCase("y")) {
+                this.bet *= 2;
+                break;
+
+            } else if (response.equalsIgnoreCase("n")) {
+                System.out.println("Goodbye!");
+                break;
+
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
     }
 
     public void makeDecision(Deck deck) {
-        Scanner sc = new Scanner(System.in);
-
-        while(true) {
+        while (true) {
             System.out.println("Hit or stand?");
-            String input = sc.nextLine();
+            String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("stand")) {
                 return;
@@ -31,7 +68,4 @@ public class Player extends Person {
             }
         }
     }
-
-    }
-
-
+}
